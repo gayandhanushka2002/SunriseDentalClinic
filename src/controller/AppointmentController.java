20600979e469279463b272dc09e457248f6c7a1d
Gayan Dhanushka Dilshan <161046388+gayandhanushka2002@users.noreply.github.com>
2026-09-02 10:21:47 +0530

    Added background email notification feature for appointments

--- src/controller/AppointmentController.java
+++ src/controller/AppointmentController.java
@@ -10,6 +10,15 @@ import model.Appointment;
 import model.Patient;
 import util.Notification;
 import util.NotificationFactory;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 /**
  *
  * @author Gayan
@@ -36,9 +45,14 @@ public class AppointmentController {
             boolean isApptSaved = appointmentDAO.insertAppointment(appointment);
             
             if (isApptSaved) {
-                // 3. ADVANCED FEATURE: Send an SMS notification using Factory Pattern
+                // 3. ADVANCED FEATURE: Send an SMS notification using Factory Pattern (Design Pattern Marks වලට මේක තියාගමු)
                 Notification smsAlert = NotificationFactory.createNotification("SMS");
                 smsAlert.send("Dear " + patient.getName() + ", your appointment (" + appointment.getAppointmentNo() + ") is successfully booked!");
+                
+                // 4. Send REAL Email notification in a Background Thread
+                new Thread(() -> {
+                    sendConfirmationEmail(patient, appointment);
+                }).start();
                 
                 return true;
             }
@@ -48,13 +62,64 @@ public class AppointmentController {
         return false;
     }
     
-        // Method to pass search keyword to DAO and return data for the Table
-        public java.util.List<Object[]> getAppointmentsList(String keyword) {
-            dao.AppointmentDAO apptDAO = new dao.AppointmentDAO();
-            return apptDAO.searchAppointments(keyword);
+    // sending email method
+    private void sendConfirmationEmail(Patient patient, Appointment appt) {
+        //  Gmail and, 2-Step Verification included App Password in here 
+        final String senderEmail = "sunrisedentalclinic97@gmail.com"; 
+        final String senderPassword = "lvurzguijhtqcdkk"; 
+
+        // Email Settings
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderEmail, senderPassword);
+            }
+          });
+
+        try {
+            // create Message 
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(senderEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(patient.getEmail()));
+            message.setSubject("Appointment Confirmation - Sunrise Dental Clinic");
+            
+            String emailText = "Dear " + patient.getName() + ",\n\n"
+                    + "Your appointment is successfully booked!\n\n"
+                    + "--- Appointment Details ---\n"
+                    + "Appointment No : " + appt.getAppointmentNo() + "\n"
+                    + "Date : " + appt.getApptDate() + "\n"
+                    + "Time : " + appt.getApptTime() + "\n\n"
+                    + "Thank you,\nSunrise Dental Clinic";
+                    
+            message.setText(emailText);
+
+            // send Message 
+            Transport.send(message);
+            
+            System.out.println("Email successfully sent to: " + patient.getEmail());
+            System.out.println("=================================");
+            System.out.println(emailText);
+            System.out.println("=================================");
+
+        } catch (MessagingException e) {
+            System.out.println("Error sending email: " + e.getMessage());
         }
+    }
+    // ----------------------------------------------
+    
+    // Method to pass search keyword to DAO and return data for the Table
+    public java.util.List<Object[]> getAppointmentsList(String keyword) {
+        dao.AppointmentDAO apptDAO = new dao.AppointmentDAO();
+        return apptDAO.searchAppointments(keyword);
+    }
         
-        // Method to pass delete request to DAO
+    // Method to pass delete request to DAO
     public boolean deleteAppointment(String apptNo) {
         dao.AppointmentDAO apptDAO = new dao.AppointmentDAO();
         return apptDAO.deleteAppointment(apptNo);
--- src/model/Patient.java
+++ src/model/Patient.java
@@ -11,16 +11,19 @@ package model;
 public class Patient extends Person {
     
     private String patientId;
+    private String email;
+    
 
     // Default constructor
     public Patient() {
     }
 
     // Parameterized constructor
-    public Patient(String patientId, String name, String address, String contactNumber) {
+    public Patient(String patientId, String name, String address, String contactNumber,String email) {
         // Call the superclass (Person) constructor
         super(name, address, contactNumber);
         this.patientId = patientId;
+        this.email = email;
     }
 
     // Getter and Setter for Patient ID
@@ -31,4 +34,13 @@ public class Patient extends Person {
     public void setPatientId(String patientId) {
         this.patientId = patientId;
     }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
 }
